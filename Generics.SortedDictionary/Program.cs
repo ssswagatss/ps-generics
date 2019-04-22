041a00575c35f37957d78b8cb86ff6d24890dcdc
Swagat Swain <swagat@mindfiresolutions.com>
2019-04-22 16:39:20 +0530

    IEqualityComparer ND IComparer

--- Generics.SortedDictionary/Program.cs
+++ Generics.SortedDictionary/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generics.SortedDictionary
 {
@@ -10,6 +7,57 @@ namespace Generics.SortedDictionary
     {
         static void Main(string[] args)
         {
+            var people = new SortedDictionary<string, SortedSet<Person>>();
+            people.Add("scienct", new SortedSet<Person>(new PersonComparer()));
+            people["scienct"].Add(new Person("Swagat"));
+            people["scienct"].Add(new Person("Rakesh"));
+            people["scienct"].Add(new Person("Abinash"));
+            people["scienct"].Add(new Person("Abinash"));
+
+
+            people.Add("math", new SortedSet<Person>(new PersonComparer()));
+            people["math"].Add(new Person("Naibedya"));
+            people["math"].Add(new Person("Disha"));
+            people["math"].Add(new Person("Gayatri"));
+            people["math"].Add(new Person("Gayatri"));
+
+
+            foreach (var d in people)
+            {
+                Console.WriteLine($"{d.Key}");
+                foreach (var item in d.Value)
+                {
+                    Console.WriteLine($"\t{item.FirstName}");
+                }
+            }
+
+            Console.ReadKey();
+
         }
+    }
+    public class PersonComparer : IEqualityComparer<Person>, IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return string.Compare(x.FirstName, y.FirstName);
+        }
+
+        public bool Equals(Person x, Person y)
+        {
+            return string.Equals(x.FirstName, y.FirstName);
+        }
+
+        public int GetHashCode(Person obj)
+        {
+            return obj.FirstName.GetHashCode();
+        }
+    }
+    public class Person
+    {
+        public Person(string name)
+        {
+            FirstName = name;
+        }
+        public string FirstName { get; set; }
     }
 }
